98adb07d96bd091e27a2c6e24f66d864331a4296
Kane York <kyork@student.42.us.org>
2018-05-04 16:11:50 -0700

    md5: commit debugging prints before removing them

--- src/md5/block.c
+++ src/md5/block.c
@@ -6,22 +6,25 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/25 21:01:09 by kyork             #+#    #+#             */
-/*   Updated: 2018/04/25 21:14:04 by kyork            ###   ########.fr       */
+/*   Updated: 2018/05/04 14:50:00 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "md5_core.h"
 #include <libft.h>
+#include <ft_printf.h>
 #include <stdalign.h>
 
 t_u32					md5_func_f(t_u32 x, t_u32 y, t_u32 z)
 {
-	return (x & y) | (~x & z);
+	return (((y ^ z) & x) ^ z);
+	//return (x & y) | (~x & z);
 }
 
 t_u32					md5_func_g(t_u32 x, t_u32 y, t_u32 z)
 {
-	return (x & z) | (y & ~z);
+	return (((x ^ y) & z) ^ y);
+	//return (x & z) | (y & ~z);
 }
 
 t_u32					md5_func_h(t_u32 x, t_u32 y, t_u32 z)
@@ -53,10 +56,17 @@ void					md5_block(t_md5state *st, t_u8 *buf)
 		x = &copy;
 	}
 	ft_memcpy(save, st->s, sizeof(t_u32) * 4);
+	ft_printf("[CHUNK] %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X %08X\n",
+			x->x[0], x->x[1], x->x[2], x->x[3], x->x[4], x->x[5], x->x[6], x->x[7], x->x[8], x->x[9], x->x[10], x->x[11], x->x[12], x->x[13], x->x[14], x->x[15]);
+	ft_printf("[STATE0] %08X %08X %08X %08X\n", st->s[0], st->s[1], st->s[2], st->s[3]);
 	md5_round(st, x, &g_md5_rounds[0]);
+	ft_printf("[STATE1] %08X %08X %08X %08X\n", st->s[0], st->s[1], st->s[2], st->s[3]);
 	md5_round(st, x, &g_md5_rounds[1]);
+	ft_printf("[STATE2] %08X %08X %08X %08X\n", st->s[0], st->s[1], st->s[2], st->s[3]);
 	md5_round(st, x, &g_md5_rounds[2]);
+	ft_printf("[STATE3] %08X %08X %08X %08X\n", st->s[0], st->s[1], st->s[2], st->s[3]);
 	md5_round(st, x, &g_md5_rounds[3]);
+	ft_printf("[STATE4] %08X %08X %08X %08X\n", st->s[0], st->s[1], st->s[2], st->s[3]);
 	st->s[0] += save[0];
 	st->s[1] += save[1];
 	st->s[2] += save[2];
